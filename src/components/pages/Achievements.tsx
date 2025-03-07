5aa6f367cdd8d65ef2917d3c018ca9c69ea1a920
amelia-liston <aliston@bu.edu>
2025-03-06 19:41:37 -0500

    Added more content

--- src/components/pages/Achievements.tsx
+++ src/components/pages/Achievements.tsx
@@ -1,12 +1,51 @@
 import StyledMain from '../styled-components/StyledMain';
+import styled from 'styled-components';
+
+const StyledAwardSection=styled.div`
+    border-left: 6px solid green;
+    text-align: left;
+    margin: 0 1vw;
+    padding: 0 2vw;
+`;
+
+const StyledList=styled.ul`
+    text-align: left;
+`;
 
 export default function Achievements() {
     return(
         <>
             <StyledMain>
-                <h1>Awards</h1>
-                <h3><i>Judges Choice: Innovation Award</i></h3>
-                <p>Won the Innovation Award after presenting the project to judges for the impressive idea, technical execution, and UI/UX design.</p>
+                <h2>Awards</h2>
+                <StyledAwardSection>
+                    <h3><i>Judges Choice: Innovation Award</i></h3>
+                    <p>Won the Innovation Award after presenting the project to judges for the impressive idea, technical execution, and UI/UX design.</p>
+                </StyledAwardSection>
+                <br/>
+                <StyledAwardSection>
+                    <h3><i>Best Hack Overall</i></h3>
+                    <p>Won Best Overall Hack for project created with 5 team members in response to solving sustainability issues in food waste.</p>
+                </StyledAwardSection>
+                <h2>Other Achievements</h2>
+                <StyledAwardSection>
+                    <h3><i>Dean's List for 5 Semesters</i></h3>
+                </StyledAwardSection>
+                <h2>Skills</h2>
+                <StyledList>
+                    <li>Swift</li>
+                    <li>Objective-C</li>
+                    <li>Python</li>
+                    <li>Java</li>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>CSS</li>
+                    <li>TypeScript</li>
+                    <li>Unix</li>
+                    <li>Excel</li>
+                    <li>Next.js</li>
+                    <li>Express</li>
+                    <li>Flask</li>
+                </StyledList>
             </StyledMain>
         </>
     );
--- src/components/pages/Employment.tsx
+++ src/components/pages/Employment.tsx
@@ -1,11 +1,77 @@
 import StyledMain from '../styled-components/StyledMain.tsx';
+import styled from 'styled-components';
+
+const StyledExperience = styled.div`
+    text-align: left;
+`;
 
 export default function Employment() {
     return(
         <>
             <StyledMain>
                 <h1>Employment</h1>
-                <p>content for employment</p>
+                <StyledExperience>
+                    <h3>Apple | Cupertino, CA | Summer 2024</h3>
+                    <h4><i>Software Engineering Intern</i></h4>
+                    <ul>
+                        <li>Developed and contributed to the development of a user-facing iOS feature for the Notes App
+                            using <strong>Swift</strong> and <strong>Objective-C</strong> to
+                            implement scalable solutions and enhance product functionality and improve user experience.
+                        </li>
+                        <li>Collaborated with cross-functional teams of designers and managers to align software
+                            features with existing customers’ needs.
+                        </li>
+                    </ul>
+                </StyledExperience>
+                <br/>
+                <StyledExperience>
+                    <h3>BU Spark! | Boston, MA | January 2023 - Present</h3>
+                    <h4><i>JEDI Program Manager & Ambassador</i></h4>
+                    <ul>
+                        <li>Led and collaborated on a cross-functional team with 5 ambassadors in the planning,
+                            execution, and facilitation of 3 DEI trainings
+                            each year for <b>80+ club leaders and Spark! staff</b>, ensuring alignment with
+                            organizational goals.
+                        </li>
+                        <li>Assesses and deliberates current and upcoming projects with <b>5 project managers</b> in
+                            weekly meetings, identifying and mitigating
+                            risks and implementing contingency plans to keep projects on track and minimize disruptions.
+                        </li>
+                        <li>Researches DEI in tech to implement and build online training module to be used by Boston
+                            University students and faculty.
+                        </li>
+                    </ul>
+                </StyledExperience>
+                <br/>
+                <StyledExperience>
+                    <h3>Girls Who Code Summer Immersion Program | Remote | Summer 2023</h3>
+                    <h4><i>Teaching Assistant</i></h4>
+                    <ul>
+                        <li>Communicated regularly with the GWC teaching team of <b>6 TAs and 1 Lead Instructor</b> to
+                            support the seamless delivery and
+                            execution of lessons, activities, and partner engagements for <b>60+ students</b> for a
+                            total of six weeks
+                        </li>
+                        <li>Responded and followed all escalation procedures for any student issues that arose during
+                            the program.
+                        </li>
+                        <li>Designed and facilitated with fellow TA a discussion of DEI in Tech with <b>40+ students</b>.
+                        </li>
+                    </ul>
+                </StyledExperience>
+                <br/>
+                <StyledExperience>
+                    <h3>Woodside Bakery & Cafe | Menlo Park, CA</h3>
+                    <h4><i>Sales Clerk / Barista</i></h4>
+                    <ul>
+                        <li>Serviced <b>20-100+ customers</b> daily with prompt and polite support in-person and via
+                            telephone.
+                        </li>
+                        <li>Operated cash register to process sales; stocked merchandise and maintained and operated
+                            espresso machine.
+                        </li>
+                    </ul>
+                </StyledExperience>
             </StyledMain>
         </>
     )
